be9b148dd08cb21ca44d4c52cdd12c1c8e16c4d0
tfli_ch <ch@tfli.co.uk>
2020-11-29 10:39:40 +0000

    use effect tutorial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 // tutorial imports
-// 1- useState import
+// 1- useState imports
 // import Setup from './tutorial/1-useState/setup/1-error-example';
 // import Final from './tutorial/1-useState/final/1-error-example';
 // import Setup from './tutorial/1-useState/setup/2-useState-basics';
@@ -10,8 +10,18 @@ import React from 'react'
 // import Final from './tutorial/1-useState/final/3-useState-array';
 // import Setup from './tutorial/1-useState/setup/4-useState-object';
 // import Final from './tutorial/1-useState/final/4-useState-object';
-import Setup from './tutorial/1-useState/setup/5-useState-counter';
+// import Setup from './tutorial/1-useState/setup/5-useState-counter';
 // import Final from './tutorial/1-useState/final/5-useState-counter';
+
+// 2 - useEffect imports
+// import Setup from './tutorial/2-useEffect/setup/1-useEffect-basics';
+// import Setup from './tutorial/2-useEffect/final/1-useEffect-basics';
+// import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup';
+// import Setup from './tutorial/2-useEffect/final/2-useEffect-cleanup';
+import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
+// import Setup from './tutorial/2-useEffect/final/3-useEffect-fetch-data';
+
+
 
 
 function App() {
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,23 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log('call useEffect',value);
+    if(value>=1){
+      document.title = `New Messages (${value})`
+    }else{
+      document.title = `React App`;
+    }
+  },[value])
+  console.log('render component',value);
+  return <>
+  <h1>{value}</h1>
+  <button className="btn" onClick={()=>{setValue(value+1)}}>click me</button>
+  <button className="btn" onClick={()=>{setValue(value-1)}}>reduce me</button>
+  <button className="btn" onClick={()=>{setValue(0)}}>Reset me</button>
+  </>;
+
 };
 
 export default UseEffectBasics;
